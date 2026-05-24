ef6ce2dd8b2037c339ea7285ea732af6f94f30d1
Mahima Sharma - BCIT <msharma163@my.bcit.ca>
2026-05-23 22:46:31 -0700

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int Trap(int[] height) {
+        int n = height.Length;
+        if (n == 0) return 0;
+
+        int[] leftMax = new int[n];
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; i++)
+            leftMax[i] = Math.Max(leftMax[i - 1], height[i]);
+
+        int[] rightMax = new int[n];
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n-2; i >= 0; i--)
+            rightMax[i] = Math.Max(rightMax[i + 1], height[i]);
+
+        int total = 0;
+        for (int i = 0; i < n; i++)
+            total += Math.Min(leftMax[i], rightMax[i]) - height[i];
+
+        return total;
+    }
+}
